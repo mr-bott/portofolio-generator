682cc6e9c31a40aa85f87a42c3e3d5e8fe90953e
murali krishna <muralikrishna8309@gmail.com>
2024-11-19 10:13:23 +0530

    fixing responsivenes 2

--- portofolio-gen/src/components/Login/index.js
+++ portofolio-gen/src/components/Login/index.js
@@ -42,6 +42,7 @@ function Login() {
                 <span className="buttonlogin_text">Continue with Google</span>
               </span>
             </button>
+            <p>Beta version</p>
           </div>
 
         </div>
