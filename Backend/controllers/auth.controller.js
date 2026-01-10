84b00b5cba24a71aaf5403e1dda959eef6ac3daf
Murali Krishna <muralikrishna8309@gmail.com>
2026-01-11 02:33:24 +0530

    google login fixed

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,31 +1,61 @@
 
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
+
+// exports.googleCallback = (req, res) => {
+//   const token = jwt.sign(
+//     { email: req.user.personalInfo.email },
+//     process.env.JWT_SECRET,
+//     { expiresIn: '1d' }
+//   );
+
+//   // JS-readable cookie
+//   res.cookie("token", token, {
+//     httpOnly: false,        // frontend can read
+//     secure: true,    // true in production (HTTPS)
+//     sameSite: "none",    // to allow cross-site
+//     maxAge: 24 * 60 * 60 * 1000,
+//   });
+
+//   res.redirect(process.env.FroentendURL);
+// };
+
+// // exports.logout = (req, res) => {
+// //   res.clearCookie("token", {
+// //     secure: true,
+// //     sameSite: "none",
+// //   });
+
+// //   req.logout?.(() => {
+// //     res.json({ message: "Logout success" });
+// //   });
+// // };
+
+const jwt = require("jsonwebtoken");
 
 exports.googleCallback = (req, res) => {
   const token = jwt.sign(
     { email: req.user.personalInfo.email },
     process.env.JWT_SECRET,
-    { expiresIn: '1d' }
+    { expiresIn: "1d" }
   );
 
-  // JS-readable cookie
   res.cookie("token", token, {
-    httpOnly: false,        // frontend can read
-    secure: true,    // true in production (HTTPS)
-    sameSite: "none",    // to allow cross-site
+    httpOnly: false,
+    secure: true,
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000,
   });
 
   res.redirect(process.env.FroentendURL);
 };
 
-// exports.logout = (req, res) => {
-//   res.clearCookie("token", {
-//     secure: true,
-//     sameSite: "none",
-//   });
+exports.logout = (req, res) => {
+  res.clearCookie("token", {
+    secure: true,
+    sameSite: "none",
+  });
 
-//   req.logout?.(() => {
-//     res.json({ message: "Logout success" });
-//   });
-// };
+  req.logout?.(() => {
+    res.json({ message: "Logout success" });
+  });
+};
