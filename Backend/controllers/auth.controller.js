780a7f1fada86e39ab90b3e4593eaad6cfaec191
Murali Krishna <muralikrishna8309@gmail.com>
2026-01-11 02:43:49 +0530

    google login fixing

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,35 +1,3 @@
-
-// const jwt = require('jsonwebtoken');
-
-// exports.googleCallback = (req, res) => {
-//   const token = jwt.sign(
-//     { email: req.user.personalInfo.email },
-//     process.env.JWT_SECRET,
-//     { expiresIn: '1d' }
-//   );
-
-//   // JS-readable cookie
-//   res.cookie("token", token, {
-//     httpOnly: false,        // frontend can read
-//     secure: true,    // true in production (HTTPS)
-//     sameSite: "none",    // to allow cross-site
-//     maxAge: 24 * 60 * 60 * 1000,
-//   });
-
-//   res.redirect(process.env.FroentendURL);
-// };
-
-// // exports.logout = (req, res) => {
-// //   res.clearCookie("token", {
-// //     secure: true,
-// //     sameSite: "none",
-// //   });
-
-// //   req.logout?.(() => {
-// //     res.json({ message: "Logout success" });
-// //   });
-// // };
-
 const jwt = require("jsonwebtoken");
 
 exports.googleCallback = (req, res) => {
@@ -38,6 +6,10 @@ exports.googleCallback = (req, res) => {
     process.env.JWT_SECRET,
     { expiresIn: "1d" }
   );
+
+   res.clearCookie("token", {
+    path: "/",
+  });
 
   res.cookie("token", token, {
     httpOnly: false,
@@ -53,6 +25,7 @@ exports.logout = (req, res) => {
   res.clearCookie("token", {
     secure: true,
     sameSite: "none",
+    path: "/"
   });
 
   req.logout?.(() => {
