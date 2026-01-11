2dd5ca4b4ddd702eef8cc7b813eb4e6cc2f4ff0c
Murali Krishna <muralikrishna8309@gmail.com>
2026-01-11 13:29:50 +0530

    limit seted to 5mb for image uploads

--- Backend/routes/upload.routes.js
+++ Backend/routes/upload.routes.js
@@ -10,7 +10,12 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB
+  },
+});
 
 router.post(
   "/image",
